6ecb5fd1f9998916b2435cf9d82ae52e60374a4b
Dhairya Tyagi <dhairyatyagi2004@gamil.com>
2026-02-01 01:45:39 +0530

    Fix json to String

--- audio-transcribe-backend/src/main/java/com/audio/transcribe/Transcriptioncontroller.java
+++ audio-transcribe-backend/src/main/java/com/audio/transcribe/Transcriptioncontroller.java
@@ -88,7 +88,7 @@ public class Transcriptioncontroller {
 
         tempFile.delete();
 
-        return ResponseEntity.ok(response.getResult().getOutput().getText()
+        return ResponseEntity.ok(response.getResult().getOutput()
 );
     } catch (Exception e) {
         e.printStackTrace(); // 👈 CRITICAL
